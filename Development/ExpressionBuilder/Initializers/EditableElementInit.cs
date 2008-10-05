76d8943d565423986c92aca48ef11267a9c6857c
SND\idof_cp <SND\idof_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2008-10-05 15:31:13 +0000

    forgot to add some files ...
    git-svn-id: https://metalinq.svn.codeplex.com/svn@23337 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ Development/ExpressionBuilder/Initializers/EditableElementInit.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Linq.Expressions;
+
+namespace ExpressionBuilder
+{
+    [DataContract]
+    public class EditableElementInit
+    {
+        protected MethodInfo _method;
+        protected EditableExpressionCollection _arguments = new EditableExpressionCollection();
+
+        [DataMember]
+        public EditableExpressionCollection Arguments { get { return _arguments; } set { _arguments = value; } }
+
+        public MethodInfo AddMethod
+        {
+            get 
+            {
+                return _method;
+            }
+            set
+            {
+                _method = value;
+            }
+        }
+
+        [DataMember]
+        public string AddMethodName
+        {
+            get
+            {
+                return _method.ToSerializableForm();
+            }
+            set
+            {
+                _method = _method.FromSerializableForm(value);
+            }
+        }
+
+        public EditableElementInit()
+        {
+
+        }
+
+        public EditableElementInit(ElementInit elmInit)
+        {
+            _method = elmInit.AddMethod;
+            foreach (Expression ex in elmInit.Arguments)
+            {
+                _arguments.Add(EditableExpression.CreateEditableExpression(ex));
+            }
+        }
+
+        public ElementInit ToElementInit()
+        {
+            return Expression.ElementInit(_method, _arguments.GetExpressions());
+        }
+    }
+}
---
+++ Development/ExpressionBuilder/Initializers/EditableMemberAssignment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Linq.Expressions;
+
+namespace ExpressionBuilder
+{
+    [DataContract]
+    public class EditableMemberAssignment : EditableMemberBinding
+    {
+        protected EditableExpression _expression;
+
+        [DataMember]
+        public EditableExpression Expression { get {return _expression;} set {_expression = value;}}
+
+        public EditableMemberAssignment ()
+	    {
+
+	    }
+
+        public EditableMemberAssignment (MemberAssignment member) : base(member.BindingType, member.Member)
+	    {
+            _expression = EditableExpression.CreateEditableExpression(member.Expression);
+	    }
+
+        public override MemberBinding ToMemberBinding()
+        {
+            return System.Linq.Expressions.Expression.Bind(_member, _expression.ToExpression());
+        }
+
+         public override MemberBindingType BindingType
+        {
+            get
+            {
+                return MemberBindingType.Assignment;
+            }
+            set
+            {
+                
+            }
+        }
+    }
+}
